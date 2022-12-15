6cd5d183d7b375d133aef42d2b8b75d1e369f231
Samat <fatdrfrog@gmail.com>
2022-12-15 18:04:34 +0600

    third task

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,17 @@ function App() {
       });
   };
 
+  const getSpecificLaunch = () => {
+    axios
+      .get("https://api.spacexdata.com/v4/launches/5eb87d46ffd86e000604b388")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Grid container spacing={2} direction="column" p={3}>
       <Grid item>
